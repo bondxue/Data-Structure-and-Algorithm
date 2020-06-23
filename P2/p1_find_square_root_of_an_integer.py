327b0032b4be632fc492d3708c76b506b1238d00
Mengheng Xue <menghengxue@gmail.com>
2020-06-23 16:38:40 -0400

    update square root of integer

--- P2/p1_find_square_root_of_an_integer.py
+++ P2/p1_find_square_root_of_an_integer.py
@@ -1,3 +1,6 @@
+import logging
+
+
 def sqrt(number):
     """
     Calculate the floored square root of a number
@@ -7,10 +10,37 @@ def sqrt(number):
     Returns:
        int: Floored Square Root
     """
-    pass
+    if not isinstance(number, int):
+        logging.error("Invalid input type. Should be integer.")
+        return
 
-print ("Pass" if  (3 == sqrt(9)) else "Fail")
-print ("Pass" if  (0 == sqrt(0)) else "Fail")
-print ("Pass" if  (4 == sqrt(16)) else "Fail")
-print ("Pass" if  (1 == sqrt(1)) else "Fail")
-print ("Pass" if  (5 == sqrt(27)) else "Fail")
+    if number < 0:
+        logging.error("Square root of negative number is undefined.")
+        return
+
+    if number == 0 or number == 1:
+        return number
+
+    start, end = 1, number
+    while start <= end:
+        mid = (start + end) // 2
+        mid_square = mid * mid
+
+        if mid_square == number:
+            return mid
+        elif mid_square < number:
+            start = mid + 1
+
+        else:
+            end = mid - 1
+    return start - 1
+
+
+print("Pass" if (3 == sqrt(9)) else "Fail")
+print("Pass" if (0 == sqrt(0)) else "Fail")
+print("Pass" if (4 == sqrt(16)) else "Fail")
+print("Pass" if (1 == sqrt(1)) else "Fail")
+print("Pass" if (5 == sqrt(27)) else "Fail")
+print("Pass" if (sqrt(None) is None) else "Fail")  # None input
+print("Pass" if (sqrt(-10) is None) else "Fail")  # negative input
+print("Pass" if (sqrt(4.2) is None) else "Fail")  # None input
